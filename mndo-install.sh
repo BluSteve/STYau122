293f1144e95da69df5b213e0ec7ddb2a12b1a080
BluSteve <billiegatesftw@gmail.com>
2021-07-13 01:37:35 +0800

    More scripts!

--- mndo-install.sh
+++ mndo-install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 mkdir ~/.jdks
-cd ~/Downloads || exit
 wget "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.11%2B9/OpenJDK11U-jdk_x64_linux_hotspot_11.0.11_9.tar.gz"
 tar -xzf OpenJDK11U-jdk_x64_linux_hotspot_11.0.11_9.tar.gz -C ~/.jdks
 export PATH=~/.jdks/jdk-11.0.11+9/bin:$PATH
 echo "export PATH=~/.jdks/jdk-11.0.11+9/bin:$PATH" >> ~/.bashrc
 rm OpenJDK11U-jdk_x64_linux_hotspot_11.0.11_9.tar.gz
 
+wget "https://blusteve.com/mndo-update.sh"
+chmod +x mndo-update.sh
+
 sudo apt install libgfortran5 htop
 java -version
+
+./mndo-update.sh
---
+++ mndo-update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget "https://blusteve.com/MNDOParam.jar" -O MNDOParam.jar
+wget "https://blusteve.com/atom_properties.json" -O atom_properties.json
+wget "https://blusteve.com/MNDOHessianUpdateData.txt" -O MNDOHessianUpdateData.txt
+wget "https://blusteve.com/mndoparams.txt" -O mndoparams.txt
+wget "https://blusteve.com/reference.txt" -O reference.txt
+wget "https://blusteve.com/input.txt" -O input.txt
