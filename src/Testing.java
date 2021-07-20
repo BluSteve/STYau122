0325489f0f383f58916ce4ca36d9f9b241ee330d
BluSteve <billiegatesftw@gmail.com>
2021-07-21 02:04:16 +0800

    gpu code

--- src/Testing.java
+++ src/Testing.java
@@ -1,13 +1,125 @@
+import jcuda.Pointer;
+import jcuda.Sizeof;
+import jcuda.jcublas.JCublas;
+import jcuda.runtime.JCuda;
 import nddoparam.GeometryOptimization;
 import nddoparam.Solution;
 import nddoparam.SolutionR;
 import nddoparam.mndo.MNDOAtom;
 import nddoparam.mndo.MNDOParams;
+import org.apache.commons.lang3.time.StopWatch;
+import org.jblas.DoubleMatrix;
 import scf.AtomHandler;
 import scf.Utils;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Testing {
 	public static void main(String[] args) {
+		try {
+			testOther();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void testOther() throws IOException {
+		JCuda.setExceptionsEnabled(true);
+//		int N = 10000;
+		for (int i = 0; i < 5; i++) {
+//				DoubleMatrix.rand(10, 10).mmul(DoubleMatrix.rand(10, 10));
+			gpuMmul(DoubleMatrix.rand(5000, 5000), DoubleMatrix.rand(5000, 5000));
+		}
+		for (int N = 1000; N <= 10000; N+=1000) {
+			System.out.println("N = " + N);
+			DoubleMatrix adm = DoubleMatrix.rand(N, N);
+			DoubleMatrix bdm = DoubleMatrix.rand(N, N);
+
+			StopWatch sw = new StopWatch();
+//			sw.start();
+//			DoubleMatrix cdm = adm.mmul(bdm);
+//			sw.stop();
+//			long cpu = sw.getTime();
+//			System.out.println("CPU: " + cpu);
+//
+//			sw.reset();
+			sw.start();
+			DoubleMatrix cdmgpu = gpuMmul(adm, bdm);
+			sw.stop();
+			long gpu = sw.getTime();
+			System.out.println("GPU: " + gpu);
+			System.out.println();
+			FileWriter fw = new FileWriter("cpuvsgpu.csv",true);
+			fw.write(N + "," + -1 + "," + gpu + "\n");
+			fw.close();
+		}
+
+//		System.out.println(cdm);
+//		System.out.println(cdmgpu);
+		JCublas.cublasShutdown();
+	}
+
+	public static double[] gpuMmul(double[] a, double[] b) {
+		int n2 = a.length;
+		int N = (int) Math.sqrt(n2);
+		double[] c = new double[n2];
+
+		Pointer gpuPointerA = new Pointer();
+		Pointer gpuPointerB = new Pointer();
+		Pointer gpuPointerC = new Pointer();
+
+		JCublas.cublasAlloc(n2, Sizeof.DOUBLE, gpuPointerA);
+		JCublas.cublasAlloc(n2, Sizeof.DOUBLE, gpuPointerB);
+		JCublas.cublasAlloc(n2, Sizeof.DOUBLE, gpuPointerC);
+
+		JCublas.cublasSetVector(n2, Sizeof.DOUBLE, Pointer.to(a), 1,
+				gpuPointerA, 1);
+		JCublas.cublasSetVector(n2, Sizeof.DOUBLE, Pointer.to(b), 1,
+				gpuPointerB, 1);
+		JCublas.cublasSetVector(n2, Sizeof.DOUBLE, Pointer.to(c), 1,
+				gpuPointerC, 1);
+
+		JCublas.cublasDgemm('n', 'n', N, N, N, 1.0, gpuPointerA, N,
+				gpuPointerB, N, 0.0, gpuPointerC, N);
+
+		JCublas.cublasGetVector(n2, Sizeof.DOUBLE, gpuPointerC, 1,
+				Pointer.to(c), 1);
+
+		JCublas.cublasFree(gpuPointerA);
+		JCublas.cublasFree(gpuPointerB);
+		JCublas.cublasFree(gpuPointerC);
+
+		return c;
+	}
+
+	public static DoubleMatrix gpuMmul(DoubleMatrix a, DoubleMatrix b) {
+		return from1d(gpuMmul(to1d(a), to1d(b)));
+	}
+
+	public static double[] to1d(DoubleMatrix dm) {
+		double[] res = new double[dm.rows * dm.rows];
+		for (int i = 0; i < dm.rows; i++) {
+			for (int j = 0; j < dm.rows; j++) {
+				res[i * dm.rows + j] = dm.get(j, i);
+			}
+		}
+		return res;
+	}
+
+	public static DoubleMatrix from1d(double[] darray) {
+		int n = (int) Math.sqrt(darray.length);
+		DoubleMatrix res = new DoubleMatrix(n, n);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				res.put(j, i, darray[i * n + j]);
+			}
+		}
+		return res;
+	}
+
+
+	private static void testMain() {
 		AtomHandler.populateAtoms();
 		MNDOParams h = new MNDOParams(2.92397599125172,
 				-6.222578482830868, 0.0, -12.200235077462583, 0.0,
