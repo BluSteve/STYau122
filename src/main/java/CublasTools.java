d2921b12d70f980d388596ab5151677c4bb22cf0
BluSteve <billiegatesftw@gmail.com>
2021-11-15 00:58:26 +0800

    Convert to gradle

---
+++ src/main/java/CublasTools.java
@@ -0,0 +1,191 @@
+import jcuda.Pointer;
+import jcuda.Sizeof;
+import jcuda.jcublas.JCublas;
+import jcuda.jcusolver.JCusolverDn;
+import jcuda.jcusolver.cusolverDnHandle;
+import jcuda.runtime.JCuda;
+import org.ejml.simple.SimpleMatrix;
+import scf.Utils;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+import static jcuda.jcublas.cublasFillMode.CUBLAS_FILL_MODE_UPPER;
+import static jcuda.jcusolver.cusolverEigMode.CUSOLVER_EIG_MODE_VECTOR;
+import static jcuda.runtime.cudaMemcpyKind.cudaMemcpyDeviceToHost;
+import static jcuda.runtime.cudaMemcpyKind.cudaMemcpyHostToDevice;
+
+public class CublasTools {
+	public static void main(String[] args) {
+		int n = 10000;
+		int n2 = n * n;
+
+		cusolverDnHandle handle = new cusolverDnHandle();
+		JCusolverDn.cusolverDnCreate(handle);
+		Random rand = new Random();
+
+		int warmupn = 30;
+		for (int i = 0; i < 5; i++) {
+			SimpleMatrix warmup =
+					SimpleMatrix.random_DDRM(warmupn, warmupn, 0, 1, rand);
+			Utils.symEigen(warmup);
+			warmup.mult(warmup);
+			getSymEigenGPU(handle, warmup);
+			gpuMmul(warmup, warmup);
+		}
+
+		for (n = 10; n <= 10; n += 5) {
+			SimpleMatrix a = SimpleMatrix.random_DDRM(n, n, 0, 1, rand);
+			SimpleMatrix b = SimpleMatrix.random_DDRM(n, n, 0, 1, rand);
+
+//			for (int i = 0; i < a.numRows(); i++) {
+//				for (int j = i; j < a.numCols(); j++) {
+//					a.set(j, i, a.get(i, j));
+//				}
+//			}
+
+//			System.out.println("a = " + a);
+
+			NanoStopWatch sw = NanoStopWatch.sw();
+			gpuMmul(a, b);
+			double gpu = sw.stop();
+			System.out.println("GPU = " + gpu);
+
+			sw.start();
+			a.mult(b);
+			double cpu = sw.stop();
+			System.out.println("CPU = " + cpu);
+
+			try {
+				FileWriter fw =
+						new FileWriter("cpuvsgpueigennomemcpy.csv", true);
+				fw.write(n + "," + cpu + "," + gpu + "\n");
+				fw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static SimpleMatrix[] getSymEigenGPU(cusolverDnHandle handle,
+												SimpleMatrix a) {
+		// note: the eigenvalues are sorted
+
+		int n2 = a.getNumElements();
+		int n = a.numRows();
+		double[] a1d = to1d(a); // input matrix in gpu format
+		double[] v = new double[n2]; // eigenvectors
+		double[] w = new double[n]; // eigenvalues
+		int[] info_gpul = new int[1];
+
+		Pointer h_A = Pointer.to(a1d); // h means cpu memory
+		Pointer h_V = Pointer.to(v);
+		Pointer h_W = Pointer.to(w);
+		Pointer h_info_gpul = Pointer.to(info_gpul);
+		Pointer d_A = new Pointer(); // d means gpu memory
+		Pointer d_V = new Pointer();
+		Pointer d_W = new Pointer();
+
+		Pointer d_work = new Pointer();
+		Pointer d_dev_info = new Pointer();
+
+		JCuda.cudaMalloc(d_A, (long) n2 * Sizeof.DOUBLE);
+		JCuda.cudaMalloc(d_W, (long) n * Sizeof.DOUBLE);
+		JCuda.cudaMalloc(d_dev_info, Sizeof.INT);
+
+		int jobz = CUSOLVER_EIG_MODE_VECTOR;
+		int uplo = CUBLAS_FILL_MODE_UPPER;
+
+		JCuda.cudaMemcpy(d_A, h_A, (long) n2 * Sizeof.DOUBLE,
+				cudaMemcpyHostToDevice); // getting input to gpu
+
+		// retrieves working space requirements
+		int[] lworkl = new int[1];
+		JCusolverDn.cusolverDnDsyevd_bufferSize(handle, jobz, uplo, n, d_A, n,
+				d_W, lworkl);
+		int lwork = lworkl[0];
+		JCuda.cudaMalloc(d_work, (long) lwork * Sizeof.DOUBLE);
+
+		JCusolverDn.cusolverDnDsyevd(handle, jobz, uplo, n, d_A, n,
+				d_W, d_work, n2, d_dev_info);
+
+		JCuda.cudaMemcpy(h_W, d_W, (long) Sizeof.DOUBLE * n,
+				cudaMemcpyDeviceToHost);
+		JCuda.cudaMemcpy(h_V, d_A, (long) Sizeof.DOUBLE * n2,
+				cudaMemcpyDeviceToHost);
+		JCuda.cudaMemcpy(h_info_gpul, d_dev_info, Sizeof.INT,
+				cudaMemcpyDeviceToHost);
+
+		JCuda.cudaFree(d_A);
+		JCuda.cudaFree(d_V);
+		JCuda.cudaFree(d_W);
+		JCuda.cudaFree(d_work);
+
+		SimpleMatrix eigenvalues = new SimpleMatrix(w.length, w.length);
+		for (int i = 0; i < w.length; i++) {
+			eigenvalues.set(i, i, w[i]);
+		}
+
+		return new SimpleMatrix[]{from1d(v), eigenvalues};
+	}
+
+	public static double[] to1d(SimpleMatrix dm) {
+		double[] res = new double[dm.numRows() * dm.numRows()];
+		for (int i = 0; i < dm.numRows(); i++) {
+			for (int j = 0; j < dm.numRows(); j++) {
+				res[i * dm.numRows() + j] = dm.get(j, i);
+			}
+		}
+		return res;
+	}
+
+	public static SimpleMatrix from1d(double[] darray) {
+		int n = (int) Math.sqrt(darray.length);
+		SimpleMatrix res = new SimpleMatrix(n, n);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				res.set(j, i, darray[i * n + j]);
+			}
+		}
+		return res;
+	}
+
+	public static double[] gpuMmul(double[] a, double[] b) {
+		int n2 = a.length;
+		int N = (int) Math.sqrt(n2);
+		double[] c = new double[n2];
+
+		Pointer gpuPointerA = new Pointer();
+		Pointer gpuPointerB = new Pointer();
+		Pointer gpuPointerC = new Pointer();
+
+		JCublas.cublasAlloc(n2, Sizeof.DOUBLE, gpuPointerA);
+		JCublas.cublasAlloc(n2, Sizeof.DOUBLE, gpuPointerB);
+		JCublas.cublasAlloc(n2, Sizeof.DOUBLE, gpuPointerC);
+
+		JCublas.cublasSetVector(n2, Sizeof.DOUBLE, Pointer.to(a), 1,
+				gpuPointerA, 1);
+		JCublas.cublasSetVector(n2, Sizeof.DOUBLE, Pointer.to(b), 1,
+				gpuPointerB, 1);
+		JCublas.cublasSetVector(n2, Sizeof.DOUBLE, Pointer.to(c), 1,
+				gpuPointerC, 1);
+
+		JCublas.cublasDgemm('n', 'n', N, N, N, 1.0, gpuPointerA, N,
+				gpuPointerB, N, 0.0, gpuPointerC, N);
+
+		JCublas.cublasGetVector(n2, Sizeof.DOUBLE, gpuPointerC, 1,
+				Pointer.to(c), 1);
+
+		JCublas.cublasFree(gpuPointerA);
+		JCublas.cublasFree(gpuPointerB);
+		JCublas.cublasFree(gpuPointerC);
+
+		return c;
+	}
+
+	public static SimpleMatrix gpuMmul(SimpleMatrix a, SimpleMatrix b) {
+		return from1d(gpuMmul(to1d(a), to1d(b)));
+	}
+
+}
---
+++ src/main/java/runcycle/input/InputHandler.java
@@ -0,0 +1,293 @@
+package runcycle.input;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import nddoparam.Solution;
+import nddoparam.am1.AM1Params;
+import nddoparam.mndo.MNDOParams;
+import scf.AtomHandler;
+import scf.AtomProperties;
+import scf.Utils;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.zip.CRC32;
+
+import static nddoparam.mndo.MNDOParams.T1ParamNums;
+import static nddoparam.mndo.MNDOParams.T2ParamNums;
+
+public class InputHandler {
+	public static RawInput ri;
+
+	public static void processInput(String path) {
+		try {
+			ri = (new Gson()).fromJson(new FileReader(path + ".json"),
+					RawInput.class);
+			for (RawMolecule rm : ri.molecules) {
+				for (int i = 0; i < rm.atoms.length; i++) {
+					rm.atoms[i].coords = Utils.bohr(rm.atoms[i].coords);
+					if (rm.expGeom != null)
+						rm.expGeom[i].coords =
+								Utils.bohr(rm.expGeom[i].coords);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void updateInput(RawInput ri, String input) {
+		for (RawMolecule rm : ri.molecules) {
+			for (int i = 0; i < rm.atoms.length; i++) {
+				rm.atoms[i].coords = Utils.debohr(rm.atoms[i].coords);
+				if (rm.expGeom != null) rm.expGeom[i].coords =
+						Utils.debohr(rm.expGeom[i].coords);
+			}
+		}
+		makeInput(ri, input);
+	}
+
+	public static void makeInput(RawInput ri, String input) {
+		GsonBuilder builder = new GsonBuilder();
+		builder.setPrettyPrinting();
+		Gson gson = builder.serializeNulls().create();
+		String jsoned = gson.toJson(ri);
+		CRC32 crc32 = new CRC32();
+		crc32.update(jsoned.getBytes(StandardCharsets.UTF_8));
+		String hash = Long.toHexString(crc32.getValue()).toUpperCase();
+		ri.hash = hash;
+
+		try {
+			FileWriter fw = new FileWriter(input + ".json");
+			Files.createDirectories(Path.of("pastinputs"));
+			FileWriter fwarchive = new FileWriter(
+					"pastinputs/" + input + "-" + hash + ".json");
+			gson.toJson(ri, fwarchive);
+			gson.toJson(ri, fw);
+			fw.close();
+			fwarchive.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void convertFromTXT(String input) {
+		AtomHandler.populateAtoms();
+		ri = new RawInput();
+		try {
+			List<String> lines = Files.readAllLines(Path.of(input));
+			List<String> datums = Files.readAllLines(Path.of("reference.txt"));
+			List<String> hud = Files.readAllLines(Path.of(
+					"mndohessian.txt"));
+			String[] mndoParamsS =
+					Files.readAllLines(Path.of("mndoparams.txt")).get(0)
+							.split(", ");
+			double[] mp = Utils.toDoubles(mndoParamsS);
+
+			ri.model = "mndo"; // TODO change this for AM1
+			ri.trainingSet = lines.get(0).split("=")[1];
+			ri.atomTypes = new int[ri.trainingSet.length()];
+			for (int x = 0; x < ri.trainingSet.length(); x++) {
+				ri.atomTypes[x] = AtomHandler.atomsMap
+						.get(Character.toString(ri.trainingSet.charAt(x)))
+						.getZ();
+			}
+			int[][] neededParams = new int[ri.atomTypes.length][];
+			int w = 0;
+			for (int atomType : ri.atomTypes) {
+				switch (ri.model) {
+					case "mndo":
+						if (atomType == 1)
+							neededParams[w] = MNDOParams.T1ParamNums;
+						else neededParams[w] = MNDOParams.T2ParamNums;
+						break;
+					case "am1":
+						if (atomType == 1)
+							neededParams[w] = AM1Params.HParamNums;
+						if (atomType == 5) neededParams[w] =
+								AM1Params.NParamNums;
+						if (atomType == 6) neededParams[w] =
+								AM1Params.CParamNums;
+						if (atomType == 8) neededParams[w] =
+								AM1Params.OParamNums;
+						break;
+				}
+				w++;
+			}
+			ri.neededParams = neededParams;
+			int PARAMLENGTH = 13;
+
+			ri.params = new RawParams();
+			ri.params.nddoParams = new double[ri.atomTypes.length][];
+			ri.params.lastHessian =
+					Utils.toDoubles(
+							hud.get(0).split(":")[1].strip().split(","));
+			ri.params.lastGradient =
+					Utils.toDoubles(
+							hud.get(1).split(":")[1].strip().split(","));
+			ri.params.lastDir =
+					Utils.toDoubles(
+							hud.get(2).split(":")[1].strip().split(","));
+			for (int x = 0; x < ri.atomTypes.length; x++) {
+				ri.params.nddoParams[x] =
+						Arrays.copyOfRange(mp, x * PARAMLENGTH,
+								x * PARAMLENGTH +
+										PARAMLENGTH);
+			}
+
+			ArrayList<RawMolecule> moleculesL = new ArrayList<>();
+
+			int i = 1;
+			try {
+				while (i < lines.size()) {
+					RawMolecule rm = new RawMolecule();
+					ArrayList<RawAtom> atomsL = new ArrayList<>();
+					ArrayList<RawAtom> expGeomL = new ArrayList<>();
+					rm.isUsing = true;
+
+					rm.restricted = lines.get(i).equals("RHF");
+					i++;
+
+					rm.charge = Integer.parseInt(lines.get(i).split("=")[1]);
+					i++;
+
+					rm.mult = Integer.parseInt(lines.get(i).split("=")[1]);
+					i++;
+
+					while (!lines.get(i).equals("---") &&
+							!lines.get(i).equals("EXPGEOM")) {
+						addAtom(atomsL, lines, i);
+						i++;
+					}
+
+					StringBuilder nameBuilder = new StringBuilder();
+					TreeMap<String, Integer> nameOccurrences =
+							new TreeMap<>(Collections.reverseOrder());
+					ArrayList<Integer> tempZs =
+							new ArrayList<>(Utils.maxAtomNum);
+					ArrayList<Integer> atomicNumbers = new ArrayList<>();
+					HashMap<Integer, int[]> tempNPs = new HashMap<>();
+					for (RawAtom a : atomsL) {
+						atomicNumbers.add(a.Z);
+						AtomProperties ap = AtomHandler.atomsMap.get(a.name);
+						rm.nElectrons += ap.getQ();
+						rm.nOrbitals += ap.getOrbitals().length;
+						if (!tempZs.contains(ap.getZ())) {
+							tempZs.add(ap.getZ());
+							if (ri.model.equals("mndo")) {
+								if (ap.getZ() == 1)
+									tempNPs.put(ap.getZ(), T1ParamNums);
+								else tempNPs.put(ap.getZ(), T2ParamNums);
+							}
+							// todo implement for am1
+						}
+						if (!nameOccurrences.containsKey(a.name))
+							nameOccurrences.put(a.name, 1);
+						else
+							nameOccurrences.put(a.name,
+									nameOccurrences.get(a.name) + 1);
+					}
+					rm.nElectrons -= rm.charge;
+					rm.atomicNumbers = Utils.toInts(atomicNumbers);
+					for (String key : nameOccurrences.keySet()) {
+						nameBuilder.append(key)
+								.append(nameOccurrences.get(key));
+					}
+					Collections.sort(tempZs);
+					int[] moleculeATs = Utils.toInts(tempZs);
+					int[][] moleculeNPs = new int[tempNPs.size()][];
+					for (int j = 0; j < moleculeNPs.length; j++) {
+						moleculeNPs[j] = tempNPs.get(tempZs.get(j));
+					}
+					rm.mnps = moleculeNPs;
+					rm.mats = moleculeATs;
+					String ruhf = rm.restricted ? "RHF" : "UHF";
+					rm.name = nameBuilder + "_" + rm.charge + "_" + ruhf;
+
+					if (lines.get(i).equals("EXPGEOM")) {
+						i++;
+						while (!lines.get(i).equals("---")) {
+							addAtom(expGeomL, lines, i);
+							i++;
+						}
+						rm.expGeom = new RawAtom[atomsL.size()];
+						for (int p = 0; p < expGeomL.size(); p++)
+							rm.expGeom[p] = expGeomL.get(p);
+					}
+					else {
+						rm.expGeom = null;
+					}
+
+					rm.atoms = new RawAtom[atomsL.size()];
+					for (int p = 0; p < atomsL.size(); p++)
+						rm.atoms[p] = atomsL.get(p);
+
+					int[] nIntegrals = Solution.getNIntegrals(rm);
+					if (rm.restricted)
+						rm.nIntegrals = nIntegrals[0];
+					else {
+						rm.nCoulombInts = nIntegrals[0];
+						rm.nExchangeInts = nIntegrals[1];
+					}
+					moleculesL.add(rm);
+					i++;
+				}
+			} catch (IndexOutOfBoundsException e) {
+				e.printStackTrace();
+			}
+			try {
+				i = 0;
+				int j = 0;
+				while (i < datums.size()) {
+					String[] ss;
+					double[] datum = new double[3];
+					datum[0] = Double.parseDouble(datums.get(i).split(" ")[1]);
+					i++;
+					ss = datums.get(i).split(" ");
+					if (ss.length == 2) datum[1] = Double.parseDouble(ss[1]);
+					i++;
+					ss = datums.get(i).split(" ");
+					if (ss.length == 2) datum[2] = Double.parseDouble(ss[1]);
+					i += 2;
+					moleculesL.get(j).datum = datum;
+					j++;
+				}
+			} catch (IndexOutOfBoundsException e) {
+				e.printStackTrace();
+			}
+			ri.molecules = new RawMolecule[moleculesL.size()];
+			for (int p = 0; p < moleculesL.size(); p++)
+				ri.molecules[p] = moleculesL.get(p);
+			for (int j = 0; j < ri.molecules.length; j++)
+				ri.molecules[j].index = j;
+			ri.nMolecules = ri.molecules.length;
+
+			makeInput(ri, "input");
+		} catch (
+				IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void addAtom(List<RawAtom> rawAtoms, List<String> lines,
+								int i) {
+		RawAtom ra = new RawAtom();
+		StringTokenizer t = new StringTokenizer(lines.get(i), " ");
+		t.nextToken();
+		ra.name = t.nextToken();
+		ra.Z = AtomHandler.atomsMap.get(ra.name).getZ();
+		for (int q = 0; q < 3; q++)
+			ra.coords[q] = Double.parseDouble(t.nextToken());
+		rawAtoms.add(ra);
+	}
+
+	public static void main(String[] args) {
+		InputHandler.convertFromTXT("inputtesting.txt");
+	}
+}
