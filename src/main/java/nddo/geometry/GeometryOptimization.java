896f54b2be0231bb3d94a5333f28316164b3c56f
BluSteve <billiegatesftw@gmail.com>
2021-12-18 20:15:53 +0800

    minor changes

--- src/main/java/nddo/geometry/GeometryOptimization.java
+++ src/main/java/nddo/geometry/GeometryOptimization.java
@@ -103,7 +103,7 @@ public abstract class GeometryOptimization {
 		}
 
 		int numIt = 0;
-		while (mag(gradient) > 1E-5) {
+		while (mag(gradient) > 1E-3) {
 			// computes new search direction
 			double lambda = lambda(h, U.transpose().mult(gradient), h.numRows() - counter);
 
