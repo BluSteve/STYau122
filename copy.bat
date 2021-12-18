899b56d8a48a86af6218157a04290a4f885474f2
BluSteve <billiegatesftw@gmail.com>
2021-12-18 19:26:24 +0800

    Scripts

--- copy.bat
+++ copy.bat
@@ -4,7 +4,6 @@ copy params.csv \\192.168.31.153\sambashare\home\steve\Downloads\mndo-server
 copy mndo-install.sh \\192.168.31.153\sambashare\home\steve\Downloads\mndo-server
 copy mndo-j.sh \\192.168.31.153\sambashare\home\steve\Downloads\mndo-server
 copy mndo-update.sh \\192.168.31.153\sambashare\home\steve\Downloads\mndo-server
-copy build\libs\MNDOParam.jar \\192.168.31.153\sambashare\home\steve\Downloads\mndo-server
-copy atom-properties.json \\192.168.31.153\sambashare\home\steve\Downloads\mndo-server
+copy build\libs\MNDOParam_proguard.jar \\192.168.31.153\sambashare\home\steve\Downloads\mndo-server\MNDOParam.jar
 
-ssh steve@35.232.10.107 -i C:\Users\billi\Documents\stevecyr "sudo ./mndo-update.sh"
+ssh steve@34.72.220.21 -i C:\Users\billi\Documents\stevecyr "sudo ./mndo-update.sh"
--- mndo-update.sh
+++ mndo-update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 url=https://mndo.blusteve.com
 wget "$url/MNDOParam.jar" -O MNDOParam.jar
-wget "$url/atom-properties.json" -O atom-properties.json
 wget "$url/molecules.txt" -O molecules.txt
 wget "$url/params.csv" -O params.csv
 wget "$url/param-numbers.csv" -O param-numbers.csv
