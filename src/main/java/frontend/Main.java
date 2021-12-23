6bfbacd3bd379f870bf6b5e5a507bbe31594cc10
BluSteve <billiegatesftw@gmail.com>
2021-12-23 18:24:31 +0800

    ready for pople testing

--- src/main/java/frontend/Main.java
+++ src/main/java/frontend/Main.java
@@ -16,10 +16,10 @@ public class Main {
 		RunInput input = TxtIO.readInput();
 		JsonIO.write(input, "input");
 
-		RunIterator iterator = new RunIterator(input, 1);
+		RunIterator iterator = new RunIterator(input, 10);
 		int i = 0;
 		for (RunOutput ro : iterator) {
-			TxtIO.updateInput(ro.getNextInput());
+//			TxtIO.updateInput(ro.getNextInput());
 			JsonIO.write(ro.getInput(), String.format("pastinputs/%04d-%s", i, ro.getInput().hash));
 			JsonIO.write(ro, String.format("outputs/%04d-%s-%s", i, ro.getInput().hash, ro.hash));
 			i++;
