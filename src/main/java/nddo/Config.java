9722cffb3297adafc5b2dd4bb6ce984831b7c591
BluSteve <billiegatesftw@gmail.com>
2022-01-06 12:31:17 +0800

    Config file

--- src/main/java/nddo/Config.java
+++ src/main/java/nddo/Config.java
@@ -8,13 +8,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Config {
-	public final double rhf_diiserror_limit = 1e-12, rhf_diiserror_tolerable = 5e-12;
-	public final int rhf_numIt_tolerable = 3000, rhf_numIt_max = 100000;
-	public final double uhf_diiserror_limit = 5E-13, uhf_diiserror_tolerable = 1e-12;
-	public final int uhf_numIt_tolerable = 3000, uhf_numIt_max = 100000;
-	public final int rhf_thiel_limit = 10000, uhf_thiel_limit = 10000;
-	public final int geom_opt_hessian_interval = 7;
-	public final String logging_level = "info";
+	public double rhf_diiserror_limit = 1e-12, rhf_diiserror_tolerable = 5e-12;
+	public int rhf_numIt_tolerable = 3000, rhf_numIt_max = 100000;
+	public double uhf_diiserror_limit = 5E-13, uhf_diiserror_tolerable = 1e-12;
+	public int uhf_numIt_tolerable = 3000, uhf_numIt_max = 100000;
+	public int rhf_thiel_limit = 10000, uhf_thiel_limit = 10000;
+	public int geom_opt_hessian_interval = 7;
+	public String logging_level = "info";
 
 	public void setLoggingLevel() {
 		Level level = Level.toLevel(logging_level);
