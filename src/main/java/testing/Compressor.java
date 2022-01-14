f65b9875ed1de80628dbd264f7f30449ebb90a19
BluSteve <billiegatesftw@gmail.com>
2022-01-15 02:50:24 +0800

    compression improved

--- src/main/java/testing/Compressor.java
+++ src/main/java/testing/Compressor.java
@@ -4,10 +4,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.zip.DataFormatException;
 import java.util.zip.Deflater;
@@ -15,51 +12,20 @@ import java.util.zip.Inflater;
 
 public class Compressor {
 	private static final Logger logger = LogManager.getLogger();
-	public static void main(String[] args) throws DataFormatException, IOException {
-		String input=  Files.readString(Path.of("remote-input.json"));
-
-
-		byte[] inputBytes = input.getBytes(StandardCharsets.UTF_8);
-		System.out.println(inputBytes.length);
-
-		int len = 100 + input.length();
-		byte[] outputBytes = new byte[len];
-		Deflater deflater = new Deflater();
-		deflater.setInput(inputBytes);
-		deflater.finish();
-		int compressedDataLength = deflater.deflate(outputBytes);
-		deflater.end();
-
-		Pair<Integer, byte[]> deflate = deflate(input);
-		System.out.println("a" + deflate.getRight().length);
-		System.out.println("b" + compressedDataLength);
-
-		Inflater inflater = new Inflater();
-		inflater.setInput(outputBytes, 0, compressedDataLength);
-		byte[] result = new byte[input.length()];
-		int resultLength = inflater.inflate(result);
-		inflater.end();
-
-		String outputString = new String(result, 0, resultLength, StandardCharsets.UTF_8);
-
-		System.out.println("a" + inflate(deflate.getLeft(), deflate.getRight()).length());
-		System.out.println("b" + outputString.length());
-		System.out.println();
-	}
+	private static final byte[] outputBytes = new byte[500_000_000]; // 500MB input file
 
 	public static Pair<Integer, byte[]> deflate(String input) {
-		logger.debug("input length: {}", input.length());
+		logger.info("input length: {}", input.length());
 
 		byte[] inputBytes = input.getBytes(StandardCharsets.UTF_8);
 
-		byte[] outputBytes = new byte[1000 + input.length()];
 		Deflater deflater = new Deflater();
 		deflater.setInput(inputBytes);
 		deflater.finish();
 		int outputLength = deflater.deflate(outputBytes);
 		deflater.end();
 
-		logger.debug("deflated length: {}", outputLength);
+		logger.info("deflated length: {}", outputLength);
 
 		return Pair.of(input.length(), Arrays.copyOfRange(outputBytes, 0, outputLength));
 	}
