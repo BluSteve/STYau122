15e7f3958b2c3b009527f4cb859be0c4a8bad028
BluSteve <billiegatesftw@gmail.com>
2022-01-28 23:15:07 +0800

    delete useless files

--- mndo-j.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget "https://mndo.blusteve.com/MNDOParam.jar" -O MNDOParam.jar
--- mndo-update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-url=https://mndo.blusteve.com
-wget "$url/MNDOParam.jar" -O MNDOParam.jar
-wget "$url/molecules.txt" -O molecules.txt
-wget "$url/params.csv" -O params.csv
-wget "$url/param-numbers.csv" -O param-numbers.csv
-wget "$url/config.json" -O config.json
