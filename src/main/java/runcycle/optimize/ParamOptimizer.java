34b9e652c55ee339499b6aaa2cb2baee889414a5
BluSteve <billiegatesftw@gmail.com>
2022-03-19 16:56:10 +0800

    rsrfo updated

--- src/main/java/runcycle/optimize/ParamOptimizer.java
+++ src/main/java/runcycle/optimize/ParamOptimizer.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.ejml.simple.SimpleMatrix;
 import runcycle.structs.LastRunInfo;
+import tools.Pow;
 import tools.Utils;
 
 public class ParamOptimizer {
@@ -15,17 +16,17 @@ public class ParamOptimizer {
 		newLri.error = error;
 
 		if (lri == null) {
-			newLri.trustRadius = 0.01;
+			newLri.trustRadius = 0.05;
 		}
 		else {
 			logger.info("Last run info: {}", lri);
 			double changeRatio = (error - lri.error) / lri.expectedChange;
 			logger.info("QA validity ratio: {}", changeRatio);
-			if (lri.stepSize * 5.0 / 4 > lri.trustRadius && changeRatio >= 0.8) {
-				newLri.trustRadius = 1.2 * lri.trustRadius;
+			if (lri.stepSize * 5.0 / 4 > lri.trustRadius && changeRatio >= 0.9 && lri.expectedChange < 5) {
+				newLri.trustRadius = 1.1 * lri.trustRadius;
 			}
-			else if (changeRatio <= 0.25) {
-				newLri.trustRadius = lri.stepSize / 2;
+			else if (changeRatio <= 0.25 || lri.expectedChange > 5) {
+				newLri.trustRadius = lri.stepSize / 1.2;
 			}
 			else {
 				newLri.trustRadius = lri.trustRadius;
@@ -85,7 +86,7 @@ public class ParamOptimizer {
 		SimpleMatrix d;
 		double ddot = 1;
 
-		while (Math.abs(alpha - oldalpha) > 1E-7 && Math.abs(ddot - trustradius * trustradius) > 1E-10) {
+		while (Math.abs(alpha - oldalpha) > 1E-9 && Math.abs(ddot - trustradius * trustradius) > 1E-15) {
 			oldalpha = alpha;
 			SimpleMatrix[] matrices = Utils.symEigen(B);
 			SimpleMatrix eigenvectors = matrices[0];
@@ -99,12 +100,12 @@ public class ParamOptimizer {
 				derivative += dots.get(0, i) * dots.get(0, i) * Math.pow(matrices[1].get(i, i) - alpha * lambda, -3);
 			}
 			derivative *= 2 * lambda / (1 + alpha * d.dot(d));
-			logger.info("Microiterations in Progress; RFO shift parameter(s) and validation check(s): {}, {}, {}", lambda, alpha, d.dot(d));
+			//logger.info("Microiterations in Progress; RFO shift parameter(s) and validation check(s): {}, {}, {}", lambda, alpha, d.dot(d));
 
 			alpha += 2 * (trustradius * Math.sqrt(d.dot(d)) - d.dot(d)) / derivative;
 		}
 
-		if (Math.abs(ddot) < 1E-10) {
+		if (Math.abs(ddot) < 1E-20) {
 			logger.warn("Microiterations did not converge; using standard RFO step (poor)");
 			return new double[] {1, RFOLambda(B, g)};
 		}
@@ -113,22 +114,67 @@ public class ParamOptimizer {
 		return new double[] {oldalpha, lambda};
 	}
 
+	private static double lambdaTRMderiv (SimpleMatrix dots, SimpleMatrix eigs, double lambda) {
+		double deriv = 0;
+		int num = 0;
+
+		if (dots.get(0) == 0) {
+			num = 1;
+		}
+
+		for (int i = num; i < eigs.numRows(); i++) {
+			deriv -= 2 * dots.get(i) * dots.get(i) / Pow.pow(eigs.get(i, i) + lambda, 3);
+		}
+
+		return deriv;
+	}
+
+	private static double TRMLambda(SimpleMatrix B, SimpleMatrix g, double trustradius) {
+
+		double oldalpha = 0;
+		SimpleMatrix d;
+		double ddot = 1;
+
+		SimpleMatrix[] mats = Utils.symEigen(B);
+
+		SimpleMatrix dots = g.transpose().mult(mats[0]);
+
+		SimpleMatrix eigs = mats[1];
+
+		double alpha = - eigs.get(0, 0) + 2;
+
+		while (Math.abs(alpha - oldalpha) > 1E-9 && Math.abs(ddot - trustradius * trustradius) > 1E-15) {
+			oldalpha = alpha;
+			d = B.plus(alpha, SimpleMatrix.identity(B.numRows())).pseudoInversei().mult(g).negativei();
+			ddot = d.dot(d);
+			double derivative = lambdaTRMderiv(dots, eigs, alpha);
+			//logger.info("Microiterations in Progress; TRM shift parameter and validation check(s): {}, {}",  alpha, d.dot(d));
+
+			alpha += 2 * (trustradius * Math.sqrt(d.dot(d)) - d.dot(d)) / derivative;
+		}
+
+		if (Math.abs(ddot) < 1E-20) {
+			logger.warn("Microiterations did not converge; using standard NR step (poor)");
+			return 0;
+		}
+
+
+		return - alpha;
+	}
+
 	public double[] optimize(SimpleMatrix B, SimpleMatrix gradient) {
 		SimpleMatrix searchdir;
-		double[] ls = new double[] {1, 0};
 		try {
-			//double l = RFOLambda(B, gradient);
-			ls = RSRFOAlphaLambda(B, gradient, newLri.trustRadius);
+//			double[] ls = RSRFOAlphaLambda(B, gradient, newLri.trustRadius);
+//			double l = ls[0] * ls[1]; //RS-RFO step
+			double l = TRMLambda(B, gradient, newLri.trustRadius); //TRM
 
-			double l = ls[0] * ls[1];
-			logger.info("Aggregated RFO shift parameter: {}", l);
 			searchdir = B.plus(-l, SimpleMatrix.identity(B.numRows())).pseudoInversei().mult(gradient).negativei();
 			SimpleMatrix eigenvalues = Utils.symEigen(B)[1];
 			SimpleMatrix eig = eigenvalues.diag().transposei();
 
 			int negCount = 0;
 			for (double v : eig.getDDRM().data) if (v < 0) negCount++;
-
 			logger.info("Hessian eigenvalues ({} negative): {}", negCount, eig.toString().strip());
 		} catch (IllegalArgumentException e) {
 			e.printStackTrace();
@@ -155,7 +201,10 @@ public class ParamOptimizer {
 
 		logger.info("Final step size: {}", newLri.stepSize);
 
-		newLri.expectedChange = (searchdir.dot(gradient) + 0.5 * searchdir.transpose().mult(B).mult(searchdir).get(0)) / (1 + ls[0] * searchdir.dot(searchdir));
+		logger.info ("Gradient values: {}", gradient.toString());
+
+
+		newLri.expectedChange = (searchdir.dot(gradient) + 0.5 * searchdir.transpose().mult(B).mult(searchdir).get(0));
 
 		return changes;
 	}
